b2a90c99ed7c51bd5c04d8e5fc7d752507aa9a22
Chris Zumberge <chriszumberge@gmail.com>
2017-10-09 15:55:52 -0400

    Finished single object tracking, added code for multiple objects

---
+++ ObjectTracker/ObjectTracker/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ObjectTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageboxCamera = new Emgu.CV.UI.ImageBox();
+            this.imageboxMask = new Emgu.CV.UI.ImageBox();
+            this.imageboxTracking = new Emgu.CV.UI.ImageBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxCamera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxMask)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxTracking)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1184, 412);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.imageboxCamera);
+            this.splitContainer1.Size = new System.Drawing.Size(388, 406);
+            this.splitContainer1.SplitterDistance = 40;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(397, 3);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.imageboxMask);
+            this.splitContainer2.Size = new System.Drawing.Size(388, 406);
+            this.splitContainer2.SplitterDistance = 40;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(791, 3);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.label3);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.imageboxTracking);
+            this.splitContainer3.Size = new System.Drawing.Size(390, 406);
+            this.splitContainer3.SplitterDistance = 40;
+            this.splitContainer3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camera Frame";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mask Frame";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tracking Frame";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imageboxCamera
+            // 
+            this.imageboxCamera.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageboxCamera.Location = new System.Drawing.Point(0, 0);
+            this.imageboxCamera.Name = "imageboxCamera";
+            this.imageboxCamera.Size = new System.Drawing.Size(388, 362);
+            this.imageboxCamera.TabIndex = 2;
+            this.imageboxCamera.TabStop = false;
+            // 
+            // imageboxMask
+            // 
+            this.imageboxMask.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageboxMask.Location = new System.Drawing.Point(0, 0);
+            this.imageboxMask.Name = "imageboxMask";
+            this.imageboxMask.Size = new System.Drawing.Size(388, 362);
+            this.imageboxMask.TabIndex = 2;
+            this.imageboxMask.TabStop = false;
+            // 
+            // imageboxTracking
+            // 
+            this.imageboxTracking.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageboxTracking.Location = new System.Drawing.Point(0, 0);
+            this.imageboxTracking.Name = "imageboxTracking";
+            this.imageboxTracking.Size = new System.Drawing.Size(390, 362);
+            this.imageboxTracking.TabIndex = 2;
+            this.imageboxTracking.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 412);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "ObjectTracker";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxCamera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxMask)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageboxTracking)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private Emgu.CV.UI.ImageBox imageboxTracking;
+        private Emgu.CV.UI.ImageBox imageboxMask;
+        private Emgu.CV.UI.ImageBox imageboxCamera;
+    }
+}
+
---
+++ ObjectTracker/ObjectTracker/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectTracker
+{
+    public partial class Form1 : Form
+    {
+        static VideoCapture _camera;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //string captureSource = @"C:\Users\zumberc\Documents\Python Scripts\object_tracking_example.mp4"; // or live capture
+            string captureSource = @"C:\Users\zumberc\Documents\Python Scripts\Video.MOV";
+
+            try
+            {
+                _camera = new VideoCapture(captureSource);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+
+            // Occurs when the application finishes processing and is about to enter the idle state
+            Application.Idle += Application_Idle;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            Matrix<int> greenLower = new Matrix<int>(new int[] { 29, 86, 6 });
+            Matrix<int> greenUpper = new Matrix<int>(new int[] { 64, 255, 255 });
+
+            // grab the current frame
+            Mat frame = _camera.QueryFrame();
+
+            if (frame != null)
+            {
+                // resize the frame
+                //CvInvoke.Resize(frame, frame, new Size(600, 600));
+
+                Mat trackingFrame = new Mat();
+                frame.CopyTo(trackingFrame);
+
+                // blur the image to smooth it and reduce high frequency noise
+                Mat blurred = new Mat();
+                CvInvoke.GaussianBlur(frame, blurred, new Size(11, 11), 0);
+
+                // convert to HSV color space since we defined the green range in the HSV color space
+                Mat hsv = new Mat();
+                CvInvoke.CvtColor(frame, hsv, ColorConversion.Bgr2Hsv);
+
+                // construct a mask for the color "green", then perform a series of dilations and erotions to remove
+                // any small blobs left in the mask
+                Mat mask = new Mat();
+                CvInvoke.InRange(hsv, greenLower, greenUpper, mask);
+                // null, point(-1, 1), bordertype.constant, and morphologydefaultbordervalue are the default values according to documentation
+                // http://docs.opencv.org/2.4/modules/imgproc/doc/filtering.html?highlight=erode#erode
+                CvInvoke.Erode(mask, mask, null, new Point(-1, -1), 2, BorderType.Constant, CvInvoke.MorphologyDefaultBorderValue);
+                // 
+                // http://docs.opencv.org/2.4/modules/imgproc/doc/filtering.html?highlight=dilate#dilate
+                CvInvoke.Dilate(mask, mask, null, new Point(-1, -1), 2, BorderType.Constant, CvInvoke.MorphologyDefaultBorderValue);
+
+                // find countours in the mask and initialize teh current (x, y) center of the ball
+                Mat contoursMask = new Mat();
+                mask.CopyTo(contoursMask);
+
+
+                //http://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=findcontours#cv2.findContours
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                CvInvoke.FindContours(contoursMask, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+                // only proceed if at least one contour was found
+                if (contours.Size > 0)
+                {
+
+                    TrackMultipleSingleObjects_NoMemory(trackingFrame, contours);
+                    //TrackSingleLargestContour(trackingFrame, contours);
+                }
+
+                imageboxCamera.Image = frame;
+                imageboxMask.Image = mask;
+                imageboxTracking.Image = trackingFrame;
+            }
+            else
+            {
+                // To show nothing when it's done
+                //imageboxCamera.Image = null;
+                //imageboxMask.Image = null;
+                //imageboxTracking.Image = null;
+            }
+        }
+
+        private void TrackMultipleSingleObjects_NoMemory(Mat trackingFrame, VectorOfVectorOfPoint contours)
+        {
+            // Get and store the bounding circles for all the contours so we can figure out which contours are contained in larger ones
+            Dictionary<int, CircleF> contourCircleDict = new Dictionary<int, CircleF>();
+
+            Rectangle[] boundRect = new Rectangle[contours.Size];
+            VectorOfVectorOfPoint contours_poly = new VectorOfVectorOfPoint(contours.Size);
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                IInputArray contour = contours[i];
+
+                // enclosing circle
+                CircleF minEnclosingCircle = CvInvoke.MinEnclosingCircle(contour);
+
+                float x = minEnclosingCircle.Center.X;
+                float y = minEnclosingCircle.Center.Y;
+                float radius = minEnclosingCircle.Radius;
+
+                MCvMoments moments = CvInvoke.Moments(contour);
+                Point center = new Point((int)(moments.M10 / moments.M00), (int)(moments.M01 / moments.M00));
+
+                CircleF circle = new CircleF(center, radius);
+
+                contourCircleDict.Add(i, circle);
+
+                // bonding rectangle
+                CvInvoke.ApproxPolyDP(contour, contours_poly[i], 3, true);
+                boundRect[i] = CvInvoke.BoundingRectangle(contours_poly[i]);
+            }
+
+            _state.ObjectsInView = 0;
+            for (int i = 0; i < contours.Size; i++)
+            {
+                IInputArray contour = contours[i];
+                CircleF contourCircle = contourCircleDict[i];
+
+                // If the contour is contained in another contour, paint it red, else, blue
+                MCvScalar colorScalar;
+                if (contourCircleDict.Any(c => c.Key != i && Form1.IsInsideCircle(contourCircle, c.Value)))
+                {
+                    // red.. inner one.. ignore right now, probably noise?
+                    colorScalar = new MCvScalar(0, 0, 255);
+                }
+                else
+                {
+                    // blue
+                    colorScalar = new MCvScalar(255, 0, 0);
+                    _state.ObjectsInView++;
+                }
+
+                Point center = new Point((int)contourCircle.Center.X, (int)contourCircle.Center.Y);
+
+                if (contourCircle.Radius > 10)
+                {
+                    CvInvoke.Circle(trackingFrame, center, (int)contourCircle.Radius,
+                       colorScalar, 2);
+                    CvInvoke.Circle(trackingFrame, center, 5, new MCvScalar(0, 0, 255), -1);
+                    CvInvoke.Rectangle(trackingFrame, boundRect[i], colorScalar, 2, LineType.EightConnected, 0);
+                }
+            }
+
+            CvInvoke.PutText(trackingFrame, _state.ObjectsInView.ToString(), new Point(10, trackingFrame.Height - 10), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.65, new MCvScalar(0, 0, 255), 2);
+        }
+
+        private void TrackSingleLargestContour(Mat trackingFrame, VectorOfVectorOfPoint contours)
+        {
+            IInputArray largestContour = null;
+            double largestContourArea = double.MinValue;
+            for (int i = 0; i < contours.Size; i++)
+            {
+                IInputArray contour = contours[i];
+                double area = CvInvoke.ContourArea(contour);
+                if (area > largestContourArea)
+                {
+                    largestContour = contour;
+                    largestContourArea = area;
+                }
+            }
+
+            if (largestContour != null)
+            {
+                CircleF minEnclosingCircle = CvInvoke.MinEnclosingCircle(largestContour);
+                float x = minEnclosingCircle.Center.X;
+                float y = minEnclosingCircle.Center.Y;
+                float radius = minEnclosingCircle.Radius;
+
+                MCvMoments moments = CvInvoke.Moments(largestContour);
+                Point center = new Point((int)(moments.M10 / moments.M00), (int)(moments.M01 / moments.M00));
+
+                obj.FrameCenter = center;
+                obj.FrameRadius = (int)radius;
+
+                // only proceed if the radius meets a minimum size
+                //if (radius > 10)
+                if (obj != null && obj.FrameRadius > 10)
+                {
+                    // draw the circle and the centroid on the frame
+                    CvInvoke.Circle(trackingFrame, obj.FrameCenter, obj.FrameRadius,
+                       new MCvScalar(0, 255, 255), 2);
+                    CvInvoke.Circle(trackingFrame, obj.FrameCenter, 5, new MCvScalar(0, 0, 255), -1);
+
+                    obj.TrailPoints.Enqueue(center);
+                    while (obj.TrailPoints.Count > _buffer)
+                        obj.TrailPoints.Dequeue();
+                }
+            }
+
+            //loop over set of tracking points
+            for (int i = 1; i < obj.TrailPoints.Count; i++)
+            {
+                if (obj.TrailPoints.ElementAt(i - 1) == null || obj.TrailPoints.ElementAt(i) == null)
+                    continue;
+
+                // check to see if enough points have been accumulated
+                if ((obj.TrailPoints.Count >= 10) && (i == 1) && (obj.TrailPoints.ElementAt(obj.TrailPoints.Count - 10) != null))
+                {
+                    // compute the difference between x and y coordinate
+                    // and re-initizalize teh direciton text variables
+                    obj.dX = obj.TrailPoints.ElementAt(obj.TrailPoints.Count - 10).X - obj.TrailPoints.ElementAt(i).X;
+                    obj.dY = obj.TrailPoints.ElementAt(obj.TrailPoints.Count - 10).Y - obj.TrailPoints.ElementAt(i).Y;
+                    obj.dirX = String.Empty;
+                    obj.dirY = String.Empty;
+
+                    // ensure there is significant movement in the x-direction
+                    if (Math.Abs(obj.dX) > 20)
+                        obj.dirX = Math.Sign(obj.dX) == 1 ? "Right" : "Left";
+                    // ensure there is significant movement in the y-direection
+                    if (Math.Abs(obj.dY) > 20)
+                        obj.dirY = Math.Sign(obj.dY) == 1 ? "Down" : "Up";
+
+                    // handle when both directions are non-empty
+                    if (!String.IsNullOrEmpty(obj.dirX) && !String.IsNullOrEmpty(obj.dirY))
+                        obj.Direction = $"{obj.dirY}-{obj.dirX}";
+                    // otherwise, only one direction is non-empty
+                    else
+                        obj.Direction = String.IsNullOrEmpty(obj.dirY) ? obj.dirX : obj.dirY;
+                }
+
+                // compute the thickness of the line and draw the connecting lines
+                int thickness = (int)(Math.Sqrt(_buffer / ((float)(i + 1))) * 2.5);
+                CvInvoke.Line(trackingFrame, obj.TrailPoints.ElementAt(i - 1), obj.TrailPoints.ElementAt(i), new MCvScalar(0, 0, 255), thickness);
+            }
+
+            CvInvoke.PutText(trackingFrame, obj.Direction, new Point(10, 30), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.65, new MCvScalar(0, 0, 255), 2);
+            CvInvoke.PutText(trackingFrame, $"dx: {obj.dX}, dy: {obj.dY}", new Point(10, 60), Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.35, new MCvScalar(0, 0, 255), 1);
+        }
+
+        /// <summary>
+        /// Determines whether the cirlce is inside the parameter circle.
+        /// </summary>
+        /// <param name="c1">The circle to check if inside of.</param>
+        /// <returns>
+        ///   <c>true</c> if [is inside circle]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsInsideCircle(CircleF c2, CircleF c1)
+        {
+            float x1 = c1.Center.X; float y1 = c1.Center.Y; float r1 = c1.Radius;
+            float x2 = c2.Center.X; float y2 = c2.Center.Y; float r2 = c2.Radius;
+
+            var d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            if (r1 > (d + r2))
+                return true;
+            else
+                return false;
+        }
+
+        const int _buffer = 32;
+        TrackableObject obj = new TrackableObject();
+        ApplicationState _state = new ApplicationState();
+
+        class ApplicationState
+        {
+            public int ObjectsInView { get; set; } = 0;
+            public int LifetimeObjects { get; set; } = 0;
+
+        }
+
+        class TrackableObject
+        {
+            public string ObjectIdentifier { get; } = Guid.NewGuid().ToString();
+
+            public List<Point> TrackingPoints { get; } = new List<Point>();
+            public Queue<Point> TrailPoints { get; } = new Queue<Point>(_buffer);
+            public Point FrameCenter { get; set; }
+            public int FrameRadius { get; set; }
+
+            public float dX { get; set; } = 0.0f;
+            public float dY { get; set; } = 0.0f;
+            public string dirX { get; set; } = String.Empty;
+            public string dirY { get; set; } = String.Empty;
+            public string Direction = String.Empty;
+        }
+    }
+}
